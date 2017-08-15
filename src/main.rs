e10b8a449d25b963db3abfc9eac5cb3efb4c8809
Andrew Plaza <aplaza@liquidthink.net>
2017-08-15 00:08:59 -0400

    imported crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate xkbcommon;
+extern crate xcb;
+use xkbcommon::xkb;
+
+struct Keyboard {
+    conn: xcb::Connection,
+    first_xkb_event: u8,
+}
+
 fn main() {
     println!("Hello, world!");
 }
